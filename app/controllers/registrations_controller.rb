b0dc6631e1cfb111b675552255891319725a4e47
vawohlsillo <vawohl@miuandes.cl>
2019-05-16 14:57:36 -0400

    Registrations create method

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,5 +5,16 @@ class RegistrationsController < ApplicationController
 
 	def create
 	    #complete this method
+		@uid = User.last.uid + 1
+		@user = User.create(uid: @uid, user_params)
+		if @user.save			
+			redirect_to "/", notice: 'Successful registration'
+		else
+			redirect_to registrations_url, notice: @user.errors.messages
+		end 
+	end
+	
+	def user_params		
+		params.require(:registrations).permit(:name, :last_name, :email, :password, :phone)
 	end
 end
