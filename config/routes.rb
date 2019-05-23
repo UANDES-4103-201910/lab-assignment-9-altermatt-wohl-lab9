2712b373a2e3c35ad4fec93147edaef44449e92a
vawohlsillo <vawohl@miuandes.cl>
2019-05-23 18:21:10 -0400

    reestrableciendo session controller y routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, :omniauth_callbacks => "callbacks"
+  devise_for :users,:omniauth_callbacks => "callbacks"
+  #, :controllers => { sessions: 'sessions', registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 
