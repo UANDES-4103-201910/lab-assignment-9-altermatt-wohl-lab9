e46787ae833aa0f650179c0078fe0438c5bb4091
vawohlsillo <vawohl@miuandes.cl>
2019-05-24 01:03:51 -0400

    sessions controller yregistrations y otras cosas añadidas, sigue fallando en autentificacion y en los html

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users,:omniauth_callbacks => "callbacks"
-  #, :controllers => { sessions: 'sessions', registrations: 'registrations' }
+  devise_for :users,:omniauth_callbacks => "callbacks", :controllers => { :registrations => "users/registrations",
+    :sessions => "users/sessions",
+    :passwords => "users/passwords" , 
+    :confirmations => "users/confirmations" }
+
+  devise_scope :user do 
+    get 'signup',  to: 'users/registrations#new'
+    get 'signin',  to: 'users/sessions#new'
+    delete 'signout', to: 'users/sessions#destroy'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  root :to => 'home#index'
 
-  root :to => 'sessions#new'
+  #post '/login',   to: 'sessions#create', as: :log_in
+  #delete '/log_out' => 'sessions#destroy', as: :log_out
 
-  post '/login',   to: 'sessions#create', as: :log_in
-  delete '/log_out' => 'sessions#destroy', as: :log_out
-
-  get '/sign_in' => 'registrations#new', as: :registrations
-  post '/sign_in' => 'registrations#create', as: :sign_in
+  #get '/sign_in' => 'registrations#new', as: :registrations
+  #post '/sign_in' => 'registrations#create', as: :sign_in
 
   #post '/logged_in' => 'users#create', as: :logged_in
   #delete '/logged_out' => 'users#destroy', as: :logged_out
